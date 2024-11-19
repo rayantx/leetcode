9eca51575f5cffb82dda7bef1f3f799f3b930984
rayantx <rayanzax@hotmail.com>
2024-11-18 23:54:34 -0300

    FindTheIndexOfTheFirstOccurrenceInAString

---
+++ FindTheIndexOfTheFirstOccurrenceInAString/Solution1.java
@@ -0,0 +1,11 @@
+package FindTheIndexOfTheFirstOccurrenceInAString;
+
+public class Solution1 {
+    public int strStr(String haystack, String needle) {
+        if (needle.isEmpty()) return 0;
+        if (haystack.contains(needle)) {
+            return haystack.indexOf(needle);
+        }
+        return -1;
+    }
+}

1572079f911d92abd7bb84fd0825996c305263fe
rayantx <rayanzax@hotmail.com>
2024-11-25 23:52:09 -0300

    RemoveDuplicatesFromSortedList

---
+++ RemoveDuplicatesFromSortedList/Solution1.java
@@ -0,0 +1,15 @@
+package RemoveDuplicatesFromSortedList;
+
+public class Solution1 {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode current_node = head;
+        while (current_node != null && current_node.next != null) {
+            if (current_node.next.val == current_node.val) {
+                current_node.next = current_node.next.next;
+            } else {
+                current_node = current_node.next;
+            }
+        }
+        return head;
+    }
+}

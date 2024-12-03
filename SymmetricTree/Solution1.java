d14a1879ad269f12e71c50da167b4e2b3aa91ec9
rayantx <rayanzax@hotmail.com>
2024-12-02 23:34:03 -0300

    SymmetricTree

---
+++ SymmetricTree/Solution1.java
@@ -0,0 +1,14 @@
+package SymmetricTree;
+
+public class Solution1 {
+    public boolean isSymmetric(TreeNode root) {
+        return isMirror(root, root);
+    }
+
+    public boolean isMirror(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        if (p == null || q == null) return false;
+
+        return (p.val == q.val) && isMirror(p.left, q.right) && isMirror(p.right, q.left);
+    }
+}

baa421e00c29a48fb6a0eb62f803e5ccaa0ca6c3
rayantx <rayanzax@hotmail.com>
2024-12-03 11:30:38 -0300

    MaximumDepthOfBinaryTree

---
+++ MaximumDepthOfBinaryTree/Solution1.java
@@ -0,0 +1,11 @@
+package MaximumDepthOfBinaryTree;
+
+public class Solution1 {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        } else {
+            return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+        }
+    }
+}
